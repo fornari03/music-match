6a7b1dd6425e3f9ed8e19c34f65e05cbcebec105
fornari03 <221017023@aluno.unb.br>
2024-08-30 22:56:52 -0300

    feat: primeira versão da tela de sign up
    Adiciona a tela de sign up com os campos de nome, email, data de nascimento
    e senha, além do campo de incluir foto de perfil (não implementado).

--- src/screens/layouts.py
+++ src/screens/layouts.py
@@ -65,7 +65,9 @@ MDScreenManager:
             theme_text_color: "Custom"
             text_color: (1, 0, 1, 1)
             pos_hint: {"center_x": 0.5}
-            on_release: root.sign_up()
+            on_release: 
+                root.manager.transition = WipeTransition()
+                root.sign_up()
 
 <HomeScreen>:
     name: "home_screen"
@@ -112,5 +114,68 @@ MDScreenManager:
 
 <SignUpScreen>:
     name: "sign_up_screen"
+    MDBoxLayout:
+        orientation: 'vertical'
+        spacing: dp(20)
+        padding: dp(40)
         
+        MDLabel:
+            text: "Crie sua conta"
+            halign: "center"
+            theme_text_color: "Custom"
+            text_color: (1, 0, 1, 1)
+            font_style: "H4"
+        
+        MDTextField:
+            id: nome
+            hint_text: "Nome"
+            icon_right: "account"
+            mode: "rectangle"
+            size_hint_x: None
+            width: 300
+            pos_hint: {"center_x": 0.5}
+
+        MDTextField:
+            id: email
+            hint_text: "Email"
+            icon_right: "email"
+            mode: "rectangle"
+            size_hint_x: None
+            width: 300
+            pos_hint: {"center_x": 0.5}
+
+        MDTextField:
+            id: nascimento
+            hint_text: "Data de Nascimento"
+            icon_right: "calendar"
+            mode: "rectangle"
+            size_hint_x: None
+            width: 300
+            pos_hint: {"center_x": 0.5}
+            on_focus: if self.focus: root.show_date_picker()
+
+        MDTextField:
+            id: senha
+            hint_text: "Senha"
+            icon_right: "lock"
+            password: True
+            mode: "rectangle"
+            size_hint_x: None
+            width: 300
+            pos_hint: {"center_x": 0.5}
+
+        MDRaisedButton:
+            text: "Adicionar Foto de Perfil"
+            md_bg_color: (1, 0, 1, 1)
+            size_hint_x: 0.3
+            pos_hint: {"center_x": 0.5}
+            on_release: root.set_profile_pic()
+
+        MDRaisedButton:
+            text: "Cadastrar"
+            md_bg_color: (1, 0, 1, 1)
+            size_hint_x: None
+            width: 300
+            pos_hint: {"center_x": 0.5}
+            on_release: root.sign_up()    
 '''
