b2b291c1ceb94a7d0c20b8870c4ae0c760c33610
Pedro Beneveli <pedro.beneveli@gmail.com>
2024-09-07 12:41:07 -0300

    Exception Handling in the query

--- src/services/connect.py
+++ src/services/connect.py
@@ -6,6 +6,9 @@ class DBConnection:
     # metodo estatico que executa um comando sql
     # sql = comando SQL a ser executado
     # hasReturn = Flag indicando se ha um retorno do comando executado
+    # Retorno:
+    # caso ocorra um erro na execucao do comando, retorna -1
+    # caso nao ocorra um erro e hasReturn == true, retorna o retorno da execucao daquele comando SQL
     @staticmethod
     def query(sql: str, hasReturn: bool):
         conn = psycopg2.connect(**load_config())
@@ -14,7 +17,12 @@ class DBConnection:
         curs = conn.cursor()
 
         # executa o comando fornecido
-        curs.execute(sql)
+        try:
+            curs.execute(sql)
+        except Exception as erro:
+            print("Erro na execucao do SQL:", erro, ", tipo: ", type(erro))
+            return -1
+
 
         data = ()
         if hasReturn:
