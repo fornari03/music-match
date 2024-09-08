39df88b3339d029caffd952ce76fe3e3f011ede1
Pedro Beneveli <pedro.beneveli@gmail.com>
2024-09-08 18:10:43 -0300

    Assinatura dos principais metodos da Musica

---
+++ src/models/musica.py
@@ -0,0 +1,25 @@
+from ..services.connect import DBConnection
+
+class Musica:
+
+    def __init__(self):
+        #TODO
+        pass
+
+    def change_values(self):
+        #TODO
+        pass
+
+    def save(self):
+        #TODO
+        pass
+
+    @staticmethod
+    def where(data: dict):
+        #TODO
+        pass
+
+    @staticmethod
+    def getArtist(idMusic: int):
+        #TODO
+        pass
